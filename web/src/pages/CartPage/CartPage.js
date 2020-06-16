7892f82da52581be41499fcf941c097296c4119c
Forrest Hayes <forresthayes@pm.me>
2020-06-16 14:03:38 -0500

    Apply app layout to cart page

--- web/src/pages/CartPage/CartPage.js
+++ web/src/pages/CartPage/CartPage.js
@@ -1,9 +1,11 @@
+import AppLayout from 'src/layouts/AppLayout'
+
 const CartPage = () => {
   return (
-    <div>
+    <AppLayout>
       <h1>CartPage</h1>
       <p>Find me in ./web/src/pages/CartPage/CartPage.js</p>
-    </div>
+    </AppLayout>
   )
 }
 
